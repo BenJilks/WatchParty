c42aa03949e414c109367cb8fff7f661cb98b972
BenJilks <benjyjilks@gmail.com>
2023-02-21 20:46:03 +0000

    Adjust monkey positions and sizes.

--- server/stage.go
+++ server/stage.go
@@ -1,86 +1,86 @@
 package main
 
 import (
-    "fmt"
-    "math/rand"
+	"fmt"
+	"math/rand"
 )
 
 type Seat struct {
-    Row    int `json:"row"`
-    Column int `json:"column"`
+	Row    int `json:"row"`
+	Column int `json:"column"`
 }
 
 type Stage struct {
-    seatsUsed map[string]Seat
+	seatsUsed map[string]Seat
 }
 
 type StageUpdateMessage struct {
-    SeatsNotFree []Seat  `json:"seats_not_free"`
-    YourToken    *string `json:"your_token"`
-    YourSeat     Seat    `json:"your_seat"`
+	SeatsNotFree []Seat  `json:"seats_not_free"`
+	YourToken    *string `json:"your_token"`
+	YourSeat     Seat    `json:"your_seat"`
 }
 
 func (stage *Stage) UpdateMessage(yourToken *string) StageUpdateMessage {
-    var seatsNotFree []Seat
-    var yourSeat Seat
-    for token, seat := range stage.seatsUsed {
-        seatsNotFree = append(seatsNotFree, seat)
-        if yourToken != nil && token == *yourToken {
-            yourSeat = seat
-        }
-    }
+	var seatsNotFree []Seat
+	var yourSeat Seat
+	for token, seat := range stage.seatsUsed {
+		seatsNotFree = append(seatsNotFree, seat)
+		if yourToken != nil && token == *yourToken {
+			yourSeat = seat
+		}
+	}
 
-    return StageUpdateMessage{
-        SeatsNotFree: seatsNotFree,
-        YourToken:    yourToken,
-        YourSeat:     yourSeat,
-    }
+	return StageUpdateMessage{
+		SeatsNotFree: seatsNotFree,
+		YourToken:    yourToken,
+		YourSeat:     yourSeat,
+	}
 }
 
 func (stage *Stage) PlayerInSeat(seat Seat) *string {
-    for player, playerSeat := range stage.seatsUsed {
-        if playerSeat.Row == seat.Row && playerSeat.Column == seat.Column {
-            return &player
-        }
-    }
+	for player, playerSeat := range stage.seatsUsed {
+		if playerSeat.Row == seat.Row && playerSeat.Column == seat.Column {
+			return &player
+		}
+	}
 
-    return nil
+	return nil
 }
 
 func (stage *Stage) SeatForPlayer(token string) *Seat {
-    seat, found := stage.seatsUsed[token]
-    if !found {
-        return nil
-    }
+	seat, found := stage.seatsUsed[token]
+	if !found {
+		return nil
+	}
 
-    return &seat
+	return &seat
 }
 
 func (stage *Stage) PlaceViewer(token string) {
-    for {
-        row := rand.Intn(len(RowSeatCount))
-        column := rand.Intn(RowSeatCount[row])
-        seat := Seat{
-            Row:    row,
-            Column: column,
-        }
+	for {
+		row := rand.Intn(len(RowSeatCount)-2) + 2
+		column := rand.Intn(RowSeatCount[row])
+		seat := Seat{
+			Row:    row,
+			Column: column,
+		}
 
-        existingPlayer := stage.PlayerInSeat(seat)
-        if existingPlayer == nil {
-            stage.seatsUsed[token] = seat
-            break
-        }
-    }
+		existingPlayer := stage.PlayerInSeat(seat)
+		if existingPlayer == nil {
+			stage.seatsUsed[token] = seat
+			break
+		}
+	}
 
-    seat := stage.seatsUsed[token]
-    fmt.Printf("Placed '%s' in row %d, seat %d\n",
-        token, seat.Row, seat.Column)
+	seat := stage.seatsUsed[token]
+	fmt.Printf("Placed '%s' in row %d, seat %d\n",
+		token, seat.Row, seat.Column)
 }
 
 func (stage *Stage) RemovePlayer(token string) {
-    if _, has := stage.seatsUsed[token]; !has {
-        return
-    }
+	if _, has := stage.seatsUsed[token]; !has {
+		return
+	}
 
-    delete(stage.seatsUsed, token)
+	delete(stage.seatsUsed, token)
 }
