95037f54c8a40c1613e9ffaaf5c9c9276a9159ac
BenJilks <benjyjilks@gmail.com>
2023-02-22 17:38:55 +0000

    Allow changes stage scale with ctrl+shift+wheel.

--- client/src/App.vue
+++ client/src/App.vue
@@ -19,6 +19,29 @@ onMounted(async () => {
     console.error(`Failed to connect to web socket server: ${error}`)
   }
 })
+
+function zoom() {
+  let zoom = 1
+
+  window.onwheel = event => {
+    if (!event.shiftKey && !event.ctrlKey)
+      return
+
+    zoom -= event.deltaY * (1.0 / window.innerHeight) * 0.4
+    const scales: { [name: string]: number } = {
+      '--seat-height': 20 * zoom,
+      '--floor-height': 23 * zoom,
+      '--curtain-offset': -(2.0 / 9.0 * 50 * (1.0 / zoom)) + 3,
+      '--front-board-height': 6 * zoom,
+    }
+
+    for (const prop in scales) {
+      document.body.style.setProperty(prop, `${scales[prop]}vh`)
+    }
+  }
+}
+
+zoom()
 </script>
 
 <template>
