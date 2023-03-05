f657e83f991f440b0a39ec2d86285eab4c7f7e6b
BenJilks <benjyjilks@gmail.com>
2023-03-05 18:18:59 +0000

    Remove zoom functionality.
    This feature was far to buggy for the moment.

--- client/src/App.vue
+++ client/src/App.vue
@@ -8,29 +8,6 @@ import { inject, ref } from 'vue'
 
 const screen = ref<InstanceType<typeof StageScreen>>()
 const client_future = inject<Promise<SocketClient>>('client_future')!!
-
-function zoom() {
-    let zoom = 1
-
-    window.onwheel = event => {
-        if (!event.shiftKey && !event.ctrlKey)
-            return
-
-        zoom -= event.deltaY * (1.0 / window.innerHeight) * 0.4
-        const scales: { [name: string]: number } = {
-            '--seat-height': 20 * zoom,
-            '--floor-height': 23 * zoom,
-            '--curtain-offset': -(2.0 / 9.0 * 50 * (1.0 / zoom)) + 3,
-            '--front-board-height': 6 * zoom,
-        }
-
-        for (const prop in scales) {
-            document.body.style.setProperty(prop, `${scales[prop]}vh`)
-        }
-    }
-}
-
-zoom()
 </script>
 
 <template>
